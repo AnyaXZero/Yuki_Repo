bbe2e40f54957b871b309b9ff7767a829b5183c5
slayer <meerutuniversity996@gmail.com>
2025-05-20 10:54:20 +0530

    Update start.py

--- AnieXEricaMusic/utils/inline/start.py
+++ AnieXEricaMusic/utils/inline/start.py
@@ -7,10 +7,7 @@ from AnieXEricaMusic import app
 
 def start_panel(_):
     buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+"
-            ),
+
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
     ]
@@ -19,12 +16,7 @@ def start_panel(_):
 
 def private_panel(_):
     buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+",
-            )
-        ],
+
             [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
