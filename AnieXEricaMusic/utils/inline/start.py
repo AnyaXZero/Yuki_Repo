32accfd9a8522f7fc46e63a38e0ba63b65624e70
slayer <meerutuniversity996@gmail.com>
2025-05-24 09:16:39 +0530

    Update start.py

--- AnieXEricaMusic/utils/inline/start.py
+++ AnieXEricaMusic/utils/inline/start.py
@@ -1,32 +1,30 @@
+
 from pyrogram.types import InlineKeyboardButton
-
 import config
-from AnieXEricaMusic import app
-
+from Shigaraki import app
 
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
 
-
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+",
-            )
+            InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇs", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(
+                text="ᴀᴅᴅ ɪɴ ɢʀᴏᴜᴘ",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            ),
         ],
-
+        [
+            InlineKeyboardButton(text="ᴜᴛɪʟɪᴛʏ ᴄᴏᴍᴍᴀɴᴅs", callback_data="settings_back_helper"),
+        ]
     ]
     return buttons
