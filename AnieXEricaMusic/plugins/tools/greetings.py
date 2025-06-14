62a3c5a8a3ac1f10c1a60d5a3ba15f08718631ec
𝘝𝘐𝘙𝘌𝘕𝘋𝘙𝘈 <194282754+AnyaXZero@users.noreply.github.com>
2025-06-14 12:30:03 +0530

    Update greetings.py

--- AnieXEricaMusic/plugins/tools/greetings.py
+++ AnieXEricaMusic/plugins/tools/greetings.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from pyrogram.enums import ParseMode
 from AnieXEricaMusic import app
 
-@app.on_message(filters.command(["gn", "n", "oodnight", "ood Night", "ood night"], prefixes=["/", "g", "G"]))
+@app.on_message(filters.command(["gn", "n", "oodnight", "ood Night", "ood night","ud night"], prefixes=["/", "g", "G"]))
 async def goodnight_command_handler(_, message: Message):
     sender = message.from_user
     sender_firstname = f"<a href='tg://user?id={sender.id}'>{sender.first_name}</a>"
@@ -33,7 +33,7 @@ def get_random_emoji():
     return random.choice(emojis)
 
 
-@app.on_message(filters.command(["gm", "m", "morning", "oodmorning", "ood morning"], prefixes=["/", "g", "G"]))
+@app.on_message(filters.command(["gm", "m", "morning", "oodmorning", "ood morning","ud morning"], prefixes=["/", "g", "G"]))
 async def goodmorning_command_handler(_, message: Message):
     sender = message.from_user
     sender_firstname = f"<a href='tg://user?id={sender.id}'>{sender.first_name}</a>"
