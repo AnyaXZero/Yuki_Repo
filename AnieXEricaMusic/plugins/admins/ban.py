d95ad55d01fba444745194d2b60657c259597555
𝘝𝘐𝘙𝘌𝘕𝘋𝘙𝘈 <194282754+AnyaXZero@users.noreply.github.com>
2025-06-19 09:48:27 +0530

    Update ban.py

--- AnieXEricaMusic/plugins/admins/ban.py
+++ AnieXEricaMusic/plugins/admins/ban.py
@@ -20,6 +20,42 @@ async def ban_user(client, message: Message):
     except UserAdminInvalid:
         await message.reply("I can't ban an admin.")
 
+# Extract the user ID from the command or reply
+    if len(message.command) > 1:
+        if message.reply_to_message:
+            user_id = message.reply_to_message.from_user.id
+            first_name = message.reply_to_message.from_user.first_name
+            reason = message.text.split(None, 1)[1]
+        else:
+            try:
+                user_id = int(message.command[1])
+                first_name = "User"
+            except:
+                user_obj = await get_userid_from_username(message.command[1])
+                if user_obj == None:
+                    return await message.reply_text("I can't find that user")
+                user_id = user_obj[0]
+                first_name = user_obj[1]
+
+            try:
+                reason = message.text.partition(message.command[1])[2]
+            except:
+                reason = None
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        first_name = message.reply_to_message.from_user.first_name
+        reason = None
+    else:
+        await message.reply_text("Please specify a valid user or reply to that user's message")
+        return
+        
+    msg_text, result = await ban_user(user_id, first_name, admin_id, admin_name, chat_id, reason)
+    if result == True:
+        await message.reply_text(msg_text)
+    if result == False:
+        await message.reply_text(msg_text)
+
 @app.on_message(filters.command("unban") & filters.group)
 async def unban_user(client, message: Message):
     if not message.reply_to_message:
@@ -44,6 +80,43 @@ async def mute_user(client, message: Message):
         await message.reply(f"{mention(user_id, user_name)} has been muted.")
     except ChatAdminRequired:
         await message.reply("I need to be an admin with mute rights.")
+
+ # Extract the user ID from the command or reply
+    if len(message.command) > 1:
+        if message.reply_to_message:
+            user_id = message.reply_to_message.from_user.id
+            first_name = message.reply_to_message.from_user.first_name
+            reason = message.text.split(None, 1)[1]
+        else:
+            try:
+                user_id = int(message.command[1])
+                first_name = "User"
+            except:
+                user_obj = await get_userid_from_username(message.command[1])
+                if user_obj == None:
+                    return await message.reply_text("I can't find that user")
+                user_id = user_obj[0]
+                first_name = user_obj[1]
+
+            try:
+                reason = message.text.partition(message.command[1])[2]
+            except:
+                reason = None
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        first_name = message.reply_to_message.from_user.first_name
+        reason = None
+    else:
+        await message.reply_text("Please specify a valid user or reply to that user's message")
+        return
+    
+    msg_text, result = await mute_user(user_id, first_name, admin_id, admin_name, chat_id, reason)
+    if result == True:
+        await message.reply_text(msg_text)
+           
+    if result == False:
+        await message.reply_text(msg_text)
 
 @app.on_message(filters.command("unmute") & filters.group)
 async def unmute_user(client, message: Message):
